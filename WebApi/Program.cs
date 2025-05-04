97dcf4c6aa318530c2510d41e60d8adbb7921793
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-05-05 01:26:00 +0200

    feat: add Serilog

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,17 @@
 using Products;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddProductsModule(builder.Configuration);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
