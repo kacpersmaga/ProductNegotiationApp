ecc996e46dbdf7bcb42b122dd6ea4d72d0c82fb6
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-05-05 23:19:03 +0200

    feat: add ApiResponse wrapper

---
+++ Shared/API/Common/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared.API.Common;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; init; }
+    public T? Data { get; init; }
+    public string? Message { get; init; }
+
+    public static ApiResponse<T> Ok(T data) =>
+        new() { Success = true, Data = data };
+
+    public static ApiResponse<T> Fail(string message) =>
+        new() { Success = false, Message = message };
+}
