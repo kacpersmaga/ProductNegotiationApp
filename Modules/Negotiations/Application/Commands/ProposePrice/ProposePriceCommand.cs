463545f2ed333278445f1809ba51c4e8d65e1785
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-05-06 03:26:20 +0200

    feat(negotiations): add ProposePriceCommand and handler

---
+++ Modules/Negotiations/Application/Commands/ProposePrice/ProposePriceCommand.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+namespace Negotiations.Application.Commands.ProposePrice;
+
+public record ProposePriceCommand(Guid NegotiationId, decimal NewPrice) : IRequest;
---
+++ Modules/Negotiations/Application/Commands/ProposePrice/ProposePriceCommmandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Negotiations.Domain.Interfaces;
+
+namespace Negotiations.Application.Commands.ProposePrice;
+
+public class ProposePriceCommandHandler : IRequestHandler<ProposePriceCommand>
+{
+    private readonly INegotiationService _service;
+    private readonly ILogger<ProposePriceCommandHandler> _logger;
+
+    public ProposePriceCommandHandler(INegotiationService service, ILogger<ProposePriceCommandHandler> logger)
+    {
+        _service = service;
+        _logger = logger;
+    }
+
+    public async Task Handle(ProposePriceCommand request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Client proposes new price {Price} for negotiation {NegotiationId}", request.NewPrice, request.NegotiationId);
+
+        await _service.ProposePriceAsync(request.NegotiationId, request.NewPrice, cancellationToken);
+    }
+}
