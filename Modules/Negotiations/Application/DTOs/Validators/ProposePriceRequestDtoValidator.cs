b97059563e249ed5f28d176e1982879c3fef8d70
kacper <kacper.smaga@microsoft.wsei.edu.pl>
2025-05-06 03:52:25 +0200

    feat(negotiations): add FluentValidation validators for request DTOs

---
+++ Modules/Negotiations/Application/DTOs/Validators/ProposePriceRequestDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Negotiations.Application.DTOs;
+
+namespace Negotiations.Application.DTOs.Validators
+{
+    public class ProposePriceRequestDtoValidator : AbstractValidator<ProposePriceRequestDto>
+    {
+        public ProposePriceRequestDtoValidator()
+        {
+            RuleFor(x => x.NewPrice)
+                .GreaterThan(0)
+                .WithMessage("Proposed price must be greater than 0.");
+        }
+    }
+}
---
+++ Modules/Negotiations/Application/DTOs/Validators/StartNegotiationRequestDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Negotiations.Application.DTOs.Validators
+{
+    public class StartNegotiationRequestDtoValidator : AbstractValidator<StartNegotiationRequestDto>
+    {
+        public StartNegotiationRequestDtoValidator()
+        {
+            RuleFor(x => x.CustomerId)
+                .NotEmpty().WithMessage("Customer ID is required.");
+
+            RuleFor(x => x.ProductId)
+                .NotEmpty().WithMessage("Product ID is required.");
+        }
+    }
+}
